7dc8e4d9ce11d7692a5b4d60eb3efa248df484e7
Robbie Porter <rthomporter@gmail.com>
2020-01-15 02:42:33 -0600

    initial commit

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-const express = require('express');
+const express = require("express");
+
+const userRouter = require("./users/userRouter");
+
+const postRouter = require("./posts/postRouter");
 
 const server = express();
 
-server.get('/', (req, res) => {
+server.use(express.json());
+
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
